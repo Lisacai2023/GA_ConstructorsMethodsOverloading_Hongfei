a96d4dcdb4498de92f2a134d6c2b9b6ad296d145
Lisa-cai <145958405+Lisa-cai@users.noreply.github.com>
2024-01-25 23:29:50 -0800

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GA_ConstructorsMethodsOverloading_Hongfei
+{
+    public class BankAccount
+    {
+        string _accountNumber;
+        string _customerName;
+        double _balance;
+
+        public BankAccount(string accountNumber, string customerName, double balance)
+        {
+            _accountNumber = accountNumber;
+            _customerName = customerName;
+            _balance = balance;
+        }
+
+        public string AccountNumber { get => _accountNumber;}
+        public string CustomerName { get => _customerName; set => _customerName = value; }
+        public double Balance { get => _balance;}
+
+        public void Deposit(double amout)
+        {
+            if ( amout >= 0 ) 
+            {
+                _balance += amout;
+            }
+            else 
+            {
+                Console.WriteLine("Dposit amout is invalid");
+            }
+        }
+
+        public void WithDraw(double amout)
+        {
+            if (amout >0 && amout <= _balance)
+            {
+                _balance -= amout;
+            }
+            else 
+            {
+                Console.WriteLine("You cannnot not overdraw");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{_accountNumber} - {_customerName} - Current Balance:{_balance:C}";
+        }
+
+    }
+
+    
+
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace GA_ConstructorsMethodsOverloading_Hongfei
+{
+    internal class Program
+    {
+        //Week2_GA_ConstructorsMethodsOverloading
+        //Hongfei
+        //01-25-2024
+        static void Main(string[] args)
+        {
+            Car defaultCar = new Car();
+            Car makeModelCar = new Car("Honda","Pilot");
+            Car fullCar = new Car("Honda","Pilot",2024,10);
+
+            Console.WriteLine("Car infornation");
+            Console.WriteLine(defaultCar);
+            Console.WriteLine(makeModelCar);
+            Console.WriteLine(fullCar);
+
+            BankAccount account = new BankAccount("1001","Lisa Cai",20);
+            Console.WriteLine("Bank Account information");
+            Console.WriteLine(account);
+            account.Deposit(500);
+            Console.WriteLine(account);
+            account.Deposit(-1000);
+            Console.WriteLine(account);
+            account.WithDraw(300);
+            Console.WriteLine(account);
+            account.WithDraw(-500);
+            Console.WriteLine(account);
+
+
+        }
+    }
+}
